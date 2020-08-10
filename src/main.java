c1431ad013946bde0c1f8bca60a713df730b42cd
Maxim06 <Itsyboimc911@gmail.com>
2020-08-10 11:48:21 +0200

    new  code

---
+++ src/main.java
@@ -0,0 +1,25 @@
+public class main {
+    public static void main(String[] args) {
+        int sum = 0;
+        int  [][]  array =new int[10][15];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 15 ; j++) {
+                array[i][j]= j;
+                sum=sum+array[i][j];
+                System.out.print(array[i][j] +" ");
+            }
+            System.out.println();
+            System.out.println("the sum of each individual array is " + sum);
+
+ sum=0;
+            }
+        for (int i = 0; i < 10; i++) {
+
+            for (int j = 0; j < 15; j++) {
+
+            }
+            System.out.println("\n");
+        }
+        System.out.println(sum+1050);
+        }
+    }
